da6156efd7640f4e902a9b8a539d53b3b0ce1630
Howard Yen <hyen@princeton.edu>
2023-07-04 12:09:03 -0400

    bug fix in retrieval.py

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,5 @@
 wget https://huggingface.co/datasets/princeton-nlp/ALCE-data/resolve/main/ALCE-data.tar
 tar xvf ALCE-data.tar
 mv ALCE-data data
+echo "deleting tar file..."
+rm ALCE-data.tar
--- retrieval.py
+++ retrieval.py
@@ -15,8 +15,10 @@ TOPK = 100
 def bm25_sphere_retrieval(data):
     from pyserini.search import LuceneSearcher
     index_path = os.environ.get("BM25_SPHERE_PATH")
+    print("loading bm25 index, this may take a while...")
     searcher = LuceneSearcher(index_path)
 
+    print("running bm25 retrieval...")
     for d in tqdm(data):
         query = d["question"]
         try:
@@ -34,25 +36,26 @@ def bm25_sphere_retrieval(data):
             h = json.loads(str(hit.docid).strip())
             docs.append({
                 "title": h["title"],
-                "text": h.raw,
+                "text": hit.raw,
                 "url": h["url"],
             })
         d["docs"] = docs
 
-    
+
 def gtr_build_index(encoder, docs):
     with torch.inference_mode():
         embs = encoder.encode(docs, batch_size=4, show_progress_bar=True, normalize_embeddings=True)
         embs = embs.astype("float16")
-    
+
     GTR_EMB = os.environ.get("GTR_EMB")
     with open(GTR_EMB, "wb") as f:
         pickle.dump(embs, f)
     return embs
-    
+
 
 def gtr_wiki_retrieval(data):
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("loading GTR encoder...")
     encoder = SentenceTransformer("sentence-transformers/gtr-t5-xxl", device = device)
 
     questions = [d["question"] for d in data]
@@ -63,18 +66,20 @@ def gtr_wiki_retrieval(data):
     # the wikipedia split from DPR repo: https://github.com/facebookresearch/DPR
     DPR_WIKI_TSV = os.environ.get("DPR_WIKI_TSV")
     docs = []
+    print("loading wikipedia file...")
     with open(DPR_WIKI_TSV) as f:
         reader = csv.reader(f, delimiter="\t")
         for i, row in enumerate(reader):
             if i == 0:
                 continue
             docs.append(row[2] + "\n" + row[1])
- 
+
     GTR_EMB = os.environ.get("GTR_EMB")
     if not os.path.exists(GTR_EMB):
         print("gtr embeddings not found, building...")
         embs = gtr_build_index(encoder, docs)
     else:
+        print("gtr embeddings found, loading...")
         with open(GTR_EMB, "rb") as f:
             embs = pickle.load(f)
 
@@ -82,6 +87,7 @@ def gtr_wiki_retrieval(data):
 
     gtr_emb = torch.tensor(embs, dtype=torch.float16, device=device)
 
+    print("running GTR retrieval...")
     for qi, q in enumerate(tqdm(queries)):
         q = q.to(device)
         scores = torch.matmul(gtr_emb, q)
@@ -100,7 +106,7 @@ if __name__ == "__main__":
     parser.add_argument("--data_file", type=str, default=None, help="path to the data file")
     parser.add_argument("--output_file", type=str, default=None, help="same format as the data file but with the retrieved docs.")
     args = parser.parse_args()
-    
+
     with open(args.data_file) as f:
         data = json.load(f)
 
